021541a72cde3efd05f4b9a732b4c69a8280ea9d
Madhia <24397644@sun.ac.za>
2024-08-05 13:51:17 +0200

    question 1 done

--- assignment1/q1/qst1.py
+++ assignment1/q1/qst1.py
@@ -1,22 +1,28 @@
 import numpy as np
 from PIL import Image
 
-image_path = 'material/greenscreen.jpg'
+# Read in greenscreen image
+image_path = 'greenscreen.jpg'
 image = Image.open(image_path)
 img_array = np.array(image)
 
+# Seperate the channels
 image = image.convert('RGB')
-
 r, g, b = img_array[:,:,0], img_array[:,:,1], img_array[:,:,2]
 
-mask = (g >= 160) & (g <= 255)
-
-mask = mask & (g > r) & (g > b) 
-
+mask = g >= 160
+mask = mask & (r < 60) 
 inverted_mask = ~mask
 
 output = np.zeros_like(img_array)
 output[inverted_mask] = img_array[inverted_mask]
 
-result = Image.fromarray(output)
+background = Image.open('background.png')
+background = background.convert('RGB')
+background = background.resize(image.size)
+background_array = np.array(background)
+
+result_array = np.where(mask[:,:,np.newaxis], background_array, output)
+
+result = Image.fromarray(result_array)
 result.save('output.png')
