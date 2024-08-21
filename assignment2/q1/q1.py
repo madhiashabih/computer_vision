da2e1345e566fd5de6f63193a1a9a10c18150867
Madhia <24397644@sun.ac.za>
2024-08-21 11:50:39 +0200

    Report update

--- assignment2/q1/q1.py
+++ assignment2/q1/q1.py
@@ -11,22 +11,24 @@ from PIL import Image
 image = Image.open('afghanGirl.jpg')
 image = np.array(image) 
 
-s_vals = [0.5, 1, 2]
-tx_vals = [0.5, 1, 2]
-ty_vals = [0.5, 1, 2]
+s_vals = [0.5, 0, 1, 2]
+tx = 1
+ty = 1
+div = [3,4,6]
 
 for s in s_vals:
-    for tx in tx_vals:
-        for ty in ty_vals:
-            similarities = np.array([
-                [s*math.cos(math.pi/4), -s*math.sin(math.pi/4), tx],
-                [s*math.sin(math.pi/4), s*math.cos(math.pi/4),  ty],
-                [0, 0, 1]
-            ])
+    for d in div:
+        print(f"Processing s={s}, d={d}")
+        similarities = np.array([
+            [s*math.cos(math.pi/d), -s*math.sin(math.pi/d), tx],
+            [s*math.sin(math.pi/d), s*math.cos(math.pi/d),  ty],
+            [0, 0, 1]
+        ])
 
-        transformed = applyhomography(image, similarities)
-
-        output = Image.fromarray(transformed.astype('uint8'))
-        output.save(f'output/similar_{s}_{tx}_{ty}.jpg')
-
+        try:
+            transformed = applyhomography(image, similarities)
+            output = Image.fromarray(transformed.astype('uint8'))
+            output.save(f'output/similar_d:_{d}_s:_{s}_tx:_{tx}_ty_{ty}.jpg')
+        except Exception as e:
+            print(f"Error processing s={s}, d={d}: {e}")
 
