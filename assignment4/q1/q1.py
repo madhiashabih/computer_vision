d750e92867081c1421f8bea6444590b9a8cef5ca
Madhia <24397644@sun.ac.za>
2024-09-16 17:54:11 +0200

    1b done

--- assignment4/q1/q1.py
+++ assignment4/q1/q1.py
@@ -12,6 +12,38 @@ def calculate_distance(x1, y1, x2, y2):
 def filter_matches(matches, max_distance):
     return [match for match in matches if calculate_distance(*match) <= max_distance]
 
+def random_point(matches, k=4):
+    idx = random.sample(range(len(matches)), k)
+    point = [matches[i] for i in idx ]
+    return np.array(point)
+
+def get_error(points, H):
+    num_points = len(points)
+    all_p1 = np.concatenate((points[:, 0:2], np.ones((num_points, 1))), axis=1)
+    all_p2 = points[:, 2:4]
+    estimate_p2 = np.zeros((num_points, 2))
+    for i in range(num_points):
+        temp = np.dot(H, all_p1[i])
+        estimate_p2[i] = (temp/temp[2])[0:2] # set index 2 to 1 and slice the index 0, 1
+    # Compute error
+    errors = np.linalg.norm(all_p2 - estimate_p2 , axis=1) ** 2
+    return errors
+
+def homography(pairs):
+    rows = []
+    for i in range(pairs.shape[0]):
+        p1 = np.append(pairs[i][0:2], 1)
+        p2 = np.append(pairs[i][2:4], 1)
+        row1 = [0, 0, 0, p1[0], p1[1], p1[2], -p2[1]*p1[0], -p2[1]*p1[1], -p2[1]*p1[2]]
+        row2 = [p1[0], p1[1], p1[2], 0, 0, 0, -p2[0]*p1[0], -p2[0]*p1[1], -p2[0]*p1[2]]
+        rows.append(row1)
+        rows.append(row2)
+    rows = np.array(rows)
+    U, s, V = np.linalg.svd(rows)
+    H = V[-1].reshape(3, 3)
+    H = H/H[2, 2] # standardize to let w*H[2,2] = 1
+    return H
+
 def plot_matches(src_img, matches, max_distance):
     # Convert the source image to grayscale
     gray_img = cv2.cvtColor(src_img, cv2.COLOR_BGR2GRAY)
@@ -34,6 +66,30 @@ def plot_matches(src_img, matches, max_distance):
     plt.tight_layout()
     plt.show()
 
+def ransac(matches, threshold, iters):
+    num_best_inliers = 0
+    
+    for i in range(iters):
+        points = random_point(matches)
+        H = homography(points)
+        
+        #  avoid dividing by zero 
+        if np.linalg.matrix_rank(H) < 3:
+            continue
+            
+        errors = get_error(matches, H)
+        idx = np.where(errors < threshold)[0]
+        inliers = matches[idx]
+
+        num_inliers = len(inliers)
+        if num_inliers > num_best_inliers:
+            best_inliers = inliers.copy()
+            num_best_inliers = num_inliers
+            best_H = H.copy()
+
+    print("inliers/matches: {}/{}".format(num_best_inliers, len(matches)))
+    return best_inliers, best_H
+
 # Read the data from the text file
 data = []
 with open('ET/matches.txt', 'r') as file:
@@ -53,3 +109,6 @@ matches = np.hstack((src_pts, dst_pts))
 # Create a combined image with the source and destination images side-by-side
 max_distance = 10
 plot_matches(src_img, matches, max_distance)
+
+inliers, H = ransac(matches, 0.5, 2000)
+plot_matches(src_img, inliers, 1000)
